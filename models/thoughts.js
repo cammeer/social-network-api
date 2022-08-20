18f7662d89c723302724e25dc1ea230a623d3726
cardinal <caren@merzhaus.org>
2022-08-20 14:00:38 -0400

    installs and some schemas

---
+++ models/thoughts.js
@@ -0,0 +1,61 @@
+const { Schema, model, Types } = require("mongoose");
+const { thoughts } = require(".");
+const dateFormat = require("../utils/dateFormat");
+
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 500,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+}, {
+    toJSON: {
+        getters: true,
+    },
+});
+
+const thoughtsSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        maxlength: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [reactionSchema],
+}, {
+    toJSON: {
+        virtuals: true,
+        getters: true,
+    },
+    id: false,
+});
+
+// count reactions / replies
+thoughtsSchema.virtual("reactionCount").get(function() {
+    return this.reactions.length;
+});
+
+const Thoughts = model("Thoughts", thoughtsSchema);
+
+module.exports = thoughts;
